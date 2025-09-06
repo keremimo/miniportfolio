08be89b8b32d1b3100d24c0be97b9fcea1f64d4d
Kerem Kilic <git@keremk.be>
2025-09-06 12:59:20 +0200

    Add navbar, remove logout and login buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resource :session
+  # Friendly auth routes
+  get "/login", to: "sessions#new", as: :login
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: :logout
+  get "/logout", to: "sessions#destroy"
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
